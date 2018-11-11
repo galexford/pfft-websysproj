8893f40cf765f29c6eb23546282b8025488512a0
Gabrielle Ford (Alex) <fordg@rpi.edu>
2018-11-11 17:40:55 -0500

    Accounts pages added

---
+++ account.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "accounts.websys";
+$username = "root";
+$password = "rhinoorangedust";
+$dbname = "bookdaddy";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO users (fname, lname, picture, dob, email, pass)
+VALUES ('John', 'Doe', 'none', '00/00/00', 'john@example.com', 'password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
