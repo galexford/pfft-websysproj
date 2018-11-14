8d7e6f7fedab86fe9e7f3f4bd4ce5c5c919a1723
GeordieTate <tateg@rpi.edu>
2018-11-14 10:52:08 -0500

    Created addListing.php

---
+++ static/php/addlisting.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname - "";
+
+$con=mysqli_connect($servername, $username, $password, $dbname);
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+} else {
+  echo "successfully connected to MySQL.";
+}
+echo("<br>");
+
+$sql = "INSERT INTO products (name, ISBN, Description, Type, Owner, Date)
+VALUES('$_POST[itemName], $_POST[ISBN], $_POST[itemDescription], $_POST[itemType], ')";
+
+?>
