ee543b10ef2c8114a86ee7a08d6a386060709472
Gabrielle Ford (Alex) <fordg@rpi.edu>
2018-11-20 12:25:48 -0500

    JS not needed - didn't have any functionality

--- static/js/LogIn_SignUp.js
+++ static/js/LogIn_SignUp.js
@@ -1,11 +1,1 @@
-alert("hi");
-function signup2(){
-	var user = document.getElementById("uname").innerHTML;
-	alert(user);
-	var sl = user.slice(-3);
-	alert(user);
-	alert(sl);
-	if (user.slice(-3)!= ".edu"){
-		alert("Email must be .edu, not" + user.slice(-3));
-	}
-}
+
