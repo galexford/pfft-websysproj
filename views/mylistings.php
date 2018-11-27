8ce26afc9936c046f6650a17cf9b419f38e27b9f
jfang2799 <43325692+jfang2799@users.noreply.github.com>
2018-11-27 12:57:06 -0500

    Add files via upload
    Logged in User Not Set Yet, I need help with that

---
+++ views/mylistings.php
@@ -0,0 +1,52 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bookdaddy";
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+else{
+	$user = "SELECT * FROM products WHERE OwnerID =  ";
+}
+
+function display($product){
+	$text = "<tr>";
+	$text += "<td>" + $user['name'] + "</td>";
+	$text += "<td>" + $user['ISBN']+"</td>";
+	$text += "<td>" + $user['Description']+"</td>";
+	$text += "<td>" + $user['Type']+"</td>";
+	$text += "<td>" + $user['University']+"</td>";
+	$text += "<td>" + $user['OwnerID']+"</td>";
+	$text += "<td>" + $user['dateAdded']+"</td>";
+	$text +="</tr>";
+	return $text
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My Listings</title>
+		<link rel="stylesheet" href="../static/css/bootstrap.css">
+	</head>
+	<table>
+		<th>Name</th>
+		<th>ISBN</th>
+		<th>Description</th>
+		<th>Type</th>
+		<th>University</th>
+		<th>Owner</th>
+		<th>Date Added</th>
+		<?php
+			while($row = mysql_fetch_array($user)){
+				echo display($row);
+			}
+		?>
+	</table>
+</html>
