bdaa9fdd9fbbbe56cb6c66b8bd322ef87c882d21
GeordieTate <tateg@rpi.edu>
2018-12-03 16:51:17 -0500

    Updated sql query to use current parameters

--- views/mylistings.php
+++ views/mylistings.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 else{
-	$user = "SELECT * FROM products WHERE OwnerID =  ";
+	$user = "SELECT * FROM products WHERE UID =  ";
 }
 
 function display($product){
