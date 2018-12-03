5080db1d459bc545d8a9e86cab792ee6f7680f8a
GeordieTate <tateg@rpi.edu>
2018-12-03 12:52:55 -0500

    Updated addListing.js to include the ajax from addListing.html

--- static/js/addListing.js
+++ static/js/addListing.js
@@ -5,3 +5,28 @@ function showISBN() {
 function noShowISBN() {
 	document.getElementById("isbn").style.display = 'none';
 }
+
+$(document).on("click", "#submitForm", function(e) {
+  e.preventDefault();
+
+  // var name = $("#itemName").value;
+  // var type = document.querySelector('input[name = "itemType"]:checked').value;
+  // var isbn = $("#isbnText").value;
+  // var price = $("#priceFeild").value;
+  // var desc = $("itemDescription").value;
+  //{name: name, type: type, isbn: isbn, price: price, desc: desc} 
+ 
+  var formData = $("#listingForm").serializeArray();
+
+  $.ajax({
+    type: 'POST',
+    url: '/fipro/static/php/addlisting.php',
+    data: {formData},
+    success: function() {
+      alert('successfully submitted a listing!');
+    },
+    error: function (err) {
+      console.log(err);
+    },
+  })
+})
