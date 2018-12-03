a2ec0bf5ec1fa8254d9339b7b7f8522a7a343749
Gabrielle Ford (Alex) <fordg@rpi.edu>
2018-12-02 19:05:46 -0500

    Removed html

--- static/php/signup.php
+++ static/php/signup.php
@@ -57,22 +57,3 @@
     //   exit();
     // }
 ?>
-<!doctype html>
-<html>
-<head>
-  <title>Registration</title>
-</head>
-<body>
-  <h1>User Registration</h1>
-  <?php if (isset($msg)) echo "<p>$msg</p>" ?>
-  <form method="post" action="signup.php">
-	 <label for="fname">First Name: </label><input type="text" name="fname" /><br>
-	 <label for="lname">Last Name: </label><input type="text" name="lname" /><br>
-	 <label for="dob">Date of Birth (YYYY-MM-DD): </label><input type="text" name="dob" /><br>
-    <label for="username">Email: </label><input type="text" name="username" /><br>
-    <label for="pass">Password: </label><input type="password" name="pass" /><br>
-    <label for="passconfirm">Confirm Password: </label><input type="password" name="passconfirm" /><br>
-    <input type="submit" name="register" value="Register" />
-  </form>
-</body>
-</html>
