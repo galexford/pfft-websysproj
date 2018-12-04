d4d3904c376380066e87f6bb2c47765febf796d6
GeordieTate <tateg@rpi.edu>
2018-12-03 22:06:48 -0500

    updated to work with logged in user

--- views/mylistings.php
+++ views/mylistings.php
@@ -1,52 +1,69 @@
 <?php
+session_start();
+
 $host = "localhost";
 $username = "root";
 $password = "";
 $dbname = "bookdaddy";
 
-// Create connection
-$conn = new mysqli($host, $username, $password, $dbname);
+function books($con, $person) {
+	$getBook = $con->query("SELECT * FROM products WHERE type = 'book' AND UID = $person");
+	foreach ($getBook as $row) {
+		$name = $row['name'];
+		$price =  $row['price'];
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-else{
-	$user = "SELECT * FROM products WHERE UID = ".$_SESSION['uid'];
-}
-
-function display($product){
-	$text = "<tr>";
-	$text += "<td>" + $user['name'] + "</td>";
-	$text += "<td>" + $user['ISBN']+"</td>";
-	$text += "<td>" + $user['Description']+"</td>";
-	$text += "<td>" + $user['Type']+"</td>";
-	$text += "<td>" + $user['University']+"</td>";
-	$text += "<td>" + $user['OwnerID']+"</td>";
-	$text += "<td>" + $user['dateAdded']+"</td>";
-	$text +="</tr>";
-	return $text;
+		echo '<pre>';
+		echo $name . ': ' . '$'.$price;
+		echo '</pre>';
+	}
 }
 
+function furniture($con, $person) {
+	$getBook = $con->query("SELECT * FROM products WHERE type = 'furniture' AND UID = $person");
+	foreach ($getBook as $row) {
+		$name = $row['name'];
+		$price =  $row['price'];
+
+		echo '<pre>';
+		echo $name . ': ' . '$'.$price;
+		echo '</pre>';
+	}
+}
+
+function other($con, $person) {
+	$getBook = $con->query("SELECT * FROM products WHERE type = 'other' AND UID = $person");
+	foreach ($getBook as $row) {
+		$name = $row['name'];
+		$price =  $row['price'];
+
+		echo '<pre>';
+		echo $name . ': ' . '$'.$price;
+		echo '</pre>';
+	}
+}
+
+try {
+	$con = new PDO("mysql:dbname=$dbname;host=$host", $username, $password);
+	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	$data = $_POST['data'];
+
+	$person = $_SESSION['uid'];
+
+	if($data == 'book') {
+		books($con, $person);
+	}
+
+	if($data == 'furniture') {
+		furniture($con, $person);
+	}
+
+	if($data == 'other') {
+		other($con, $person);
+	}
+
+} catch(PDOException $e) {
+	echo 'ERROR: ' .  $e->getMessage();
+}
 ?>
 
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>My Listings</title>
-		<link rel="stylesheet" href="../static/css/bootstrap.css">
-	</head>
-	<table>
-		<th>Name</th>
-		<th>ISBN</th>
-		<th>Description</th>
-		<th>Type</th>
-		<th>University</th>
-		<th>Owner</th>
-		<th>Date Added</th>
-		<?php
-			while($row = mysql_fetch_array($user)){
-				echo display($row);
-			}
-		?>
-	</table>
-</html>
