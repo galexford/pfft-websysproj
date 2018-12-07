d34dfccff885ee9ef2067b470abadac57b8c9d62
GeordieTate <tateg@rpi.edu>
2018-12-07 17:56:54 -0500

    added ajax so it works

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,6 +1,25 @@
-$(document).ready(function() {
-	var data = {'register':"Register", };
-	$.post("static/php/listings.php", data, function(data) {
-	    alert("Registered");
-	});
-});
+$(document).on("click", "#submitUser", function(e) {
+  e.preventDefault();
+ 
+  var register = $("#submitUser").serialize();
+
+  var fName = $("#fname").val();
+  var lName = $("#lname").val();
+  var username = $("#username").val();
+  var dob = $("#dob").val();
+  var pass = $("#pass").val();
+  var passconfirm = $("#passconfirm").val();
+
+  $.ajax({
+    type: 'POST',
+    url: '/fipro/static/php/signup.php',
+    data: {fname: fName, lname: lName, username: username, dob: dob, pass: pass, passconfirm: passconfirm},
+    success: function() {
+      alert('Successfully registered!');
+      document.getElementById("listingForm").reset();
+    },
+    error: function (err) {
+      console.log(err);
+    },
+  })
+})
