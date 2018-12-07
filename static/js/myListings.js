072acd06f1715de7a593ac0232ee2fd919e7b208
GeordieTate <tateg@rpi.edu>
2018-12-07 17:56:42 -0500

    changed the path for the php file

--- static/js/myListings.js
+++ static/js/myListings.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 	
 	var data = {'data': "book"};
-	$.post("views/mylistings.php", data, function(data) {
+	$.post("static/php/mylistings.php", data, function(data) {
 	    $("#myBookDiv").html(data);
 	});
 
 	var data = {'data':"furniture"};
-	$.post("views/mylistings.php", data, function(data) {
+	$.post("static/php/mylistings.php", data, function(data) {
 	    $("#myFurnDiv").html(data);
 	});
 
 	var data = {'data':"other"};
-	$.post("views/mylistings.php", data, function(data) {
+	$.post("static/php/mylistings.php", data, function(data) {
 	    $("#myOtherDiv").html(data);
 	});
 });
