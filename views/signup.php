25c9bb056259d4ccdc98ee7693b9bf8822400e0c
jfang2799 <43325692+jfang2799@users.noreply.github.com>
2018-12-07 17:19:16 -0500

    Add files via upload
    Working Signup.php

---
+++ views/signup.php
@@ -0,0 +1,81 @@
+<?php
+    session_start();
+
+    // Connect to the database
+    try {
+    $dbname = 'bookdaddy';
+    $user = 'root';
+    $pass = 'password';
+    $dbconn = new PDO('mysql:host=127.0.0.1;dbname='.$dbname, $user, $pass);
+    }
+    catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+    }
+
+    if (isset($_POST['register']) && $_POST['register'] == 'Register') {
+        // Validate fields
+        // @TODO: Also check to see if duplicate usernames exist
+        if (!isset($_POST['fname']) 
+            || !isset($_POST['lname']) 
+            || !isset($_POST['dob']) 
+            || !isset($_POST['username']) 
+            || !isset($_POST['pass']) 
+            || !isset($_POST['passconfirm']) 
+            || empty($_POST['fname']) 
+            || empty($_POST['lname']) 
+            || empty($_POST['dob']) 
+            || empty($_POST['username']) 
+            || empty($_POST['pass']) 
+            || empty($_POST['passconfirm'])) {
+          $msg = "Please fill in all form fields.";
+        }
+        else if ($_POST['pass'] !== $_POST['passconfirm']) {
+          $msg = "Passwords must match.";
+        }
+        else {
+			$email = $_POST["username"];
+			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  				$msg = "Invalid email format"; 
+			}
+            else{
+                // Generate random salt
+                $salt = hash('sha256', uniqid(mt_rand(), true));
+                // Apply salt before hashing
+                $salted = hash('sha256', $salt . $_POST['pass']);
+                // Store the salt with the password, so we can apply it again and check the result
+                $stmt = $dbconn->prepare("INSERT INTO users(fname, lname, dob, username, pass, salt)
+                                  VALUES (:fname, :lname, :dob, :username, :pass, :salt)");
+                $stmt->execute(array(
+                	 ':fname'    => $_POST['fname'],
+                	 ':lname'    => $_POST['lname'],
+                	 ':dob'      => $_POST['dob'],
+                	 ':username' => $_POST['username'],
+                                   ':pass' => $salted,
+                                   ':salt' => $salt
+                                    ));
+                $msg = "Account created.";
+            }
+        }
+    }
+
+    echo $msg;
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title> CORRECT PAGE </title>
+</head>
+    <body>
+        <h1>User Registration</h1>
+        <form method="post" action="signup.php">
+            <label for="fname">First Name: </label><input type="text" name="fname" /><br>
+            <label for="lname">Last Name: </label><input type="text" name="lname" /><br>
+            <label for="dob">Date of Birth (YYYY-MM-DD): </label><input type="text" name="dob" /><br>
+            <label for="username">Email: </label><input type="text" name="username" /><br>
+            <label for="pass">Password: </label><input type="password" name="pass" /><br>
+            <label for="passconfirm">Confirm Password: </label><input type="password" name="passconfirm" /><br>
+            <input type="submit" name="register" value="Register" />
+        </form>
+    </body>
+</html>
