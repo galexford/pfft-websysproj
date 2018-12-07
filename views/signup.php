a6d9395ac775f5b0cec4f353507eecaec1480e90
GeordieTate <tateg@rpi.edu>
2018-12-07 17:57:51 -0500

    made it work along with ajax

---
+++ views/signup.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+    <body>
+        <h1>User Registration</h1>
+        <form id="userForm">
+            <label for="fname">First Name: </label><input id="fname" type="text" name="fname" /><br>
+            <label for="lname">Last Name: </label><input id="lname" type="text" name="lname" /><br>
+            <label for="dob">Date of Birth (YYYY-MM-DD): </label><input id="dob" type="text" name="dob" /><br>
+            <label for="username">Email: </label><input id="username" type="text" name="username" /><br>
+            <label for="pass">Password: </label><input id="pass" type="password" name="pass" /><br>
+            <label for="passconfirm">Confirm Password: </label><input id="passconfirm" type="password" name="passconfirm" /><br>
+            <input id="submitUser" type="submit" name="register" value="Register" />
+        </form>
+    </body>
+</html>
